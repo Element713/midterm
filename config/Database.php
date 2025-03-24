fd0ec28fe4924d43667f67241271830f12da6886
Shadow Love-Erckert <srlove@mail.fhsu.edu>
2025-03-24 15:38:13 -0500

    port update2

--- config/Database.php
+++ config/Database.php
@@ -1,28 +1,38 @@
 <?php
-
 class Database {
     private $conn;
+    private $host;
+    private $port;
+    private $dbname;
+    private $username;
+    private $password;
 
+    public function __construct(){
+        $this->host = getenv('HOST');
+        $this->port = 5432;
+        $this->dbname = getenv('DBNAME');
+        $this->username = getenv('USERNAME');
+        $this->password = getenv('PASSWORD');
+    }
 
-    //db c onnect
     public function connect() {
-        $host = getenv('HOST');
-        $db_name = getenv('DBNAME');
-        $username = getenv('USERNAME');
-        $password = getenv('PASSWORD');
+        if ($this->conn){
+            return $this->conn;
+        } else {
+            $dsn =
+"pgsql:host={$this->host};port={$this->port};dbname={$this->dbname}";
 
-        $this->conn = null;
-
-                $dsn = "pgsql:host={$this->host}; port={$this->port};dbname={$this->db_name}";
-         
-        try {
-                 $this->conn = new PDO($dsn, $this->username, $this->password);
-                 
-                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDEOxception $e){
-            echo 'connection error' . $e->getMessage();
+            try {
+                $this->conn = new PDO($dsn, $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE,
+PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die(json_encode(['message' => 'Connection Error: ' .
+$e->getMessage()]));
+            }
+            return $this->conn;
         }
-        return $this->conn;
     }
 }
 echo "Database connection established successfully.";
+?>
