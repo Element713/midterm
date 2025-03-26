1a6961a867f1f3b1bb021c76458f6bf8e81c3e2a
Shadow Love-Erckert <srlove@mail.fhsu.edu>
2025-03-26 17:52:41 -0500

    ...

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -1,46 +1,15 @@
 <?php
-
-// Headers
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-
+// Include necessary files
 include_once '../../config/Database.php';
 include_once '../../models/Quote.php';
 
-// Instantiate DB & connect
+// Instantiate Database and connect
 $database = new Database();
 $db = $database->connect();
 
 // Instantiate Quote object
 $quote = new Quote($db);
 
-// Blog quote query
-$result = $quote->read();
-// Get row count
-$num = $result->rowCount();
-
-// Check if any Quotes exist
-if ($num > 0) {
-    // Quote array
-    $quotes_arr = array();
-
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        extract($row);
-    
-        $quote_item = array(
-            'id' => isset($id) ? $id : null,
-            'quote' => isset($quote) ? html_entity_decode($quote) : null,
-            'author' => isset($author_name) ? $author_name : 'Unknown Author',
-            'category' => isset($category_name) ? $category_name : 'Uncategorized'
-        );
-    
-        array_push($quotes_arr, $quote_item);
-    }
-
-    // output JSON
-    echo json_encode($quotes_arr);
-} else {
-    // No Quotes Found - Return an empty array
-    echo json_encode(array('message' => 'No Quotes Found'));
-}
+// Get all quotes
+echo $quote->read();
 ?>
