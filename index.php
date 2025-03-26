bcfa5d973b8c7890418fc503fe6c0f0227a4359f
Shadow Love-Erckert <srlove@mail.fhsu.edu>
2025-03-26 12:14:12 -0500

    ~`

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-
-
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header("Access-Control-Allow-Credentials: true");
@@ -9,43 +7,37 @@ header("X-Frame-Options: DENY");
 header("X-XSS-Protection: 1; mode=block");
 header("X-Content-Type-Options: nosniff");
 
-// Handle preflight OPTIONS request
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method === 'OPTIONS') {
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
     exit();
 }
 
 // Load dependencies
-require_once 'config/Database.php';
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+require_once 'config/database.php';
 
 // Establish database connection
 $database = new Database();
 $db = $database->connect();
 
-// Get request URI without query params
-$request_uri = strtok($_SERVER['REQUEST_URI'], '?');
-$request_uri = str_replace('/index.php', '', $request_uri); // Remove /index.php if present
+// Handle Routing
+$request_uri = strtok($_SERVER['REQUEST_URI'], '?'); // Removes query parameters
 
-// Routing
-$routes = [
-    '/quotes' => 'routes/Quote.php',
-    '/authors' => 'routes/Author.php',
-    '/categories' => 'routes/Category.php'
-];
-
-if (isset($routes[$request_uri])) {
-    if (file_exists($routes[$request_uri])) {
-        require $routes[$request_uri];
-    } else {
-        http_response_code(500);
-        echo json_encode(['message' => 'Internal Server Error: Route file missing']);
-    }
-} else {
-    http_response_code(404);
-    echo json_encode(['message' => 'Endpoint not found']);
+switch ($request_uri) {
+    case '/quotes':
+        require 'routes/quotes.php';
+        break;
+    case '/authors':
+        require 'routes/authors.php';
+        break;
+    case '/categories':
+        require 'routes/categories.php';
+        break;
+    default:
+        http_response_code(404);
+        echo json_encode(['message' => 'Endpoint not found']);
+        break;
 }
-?>
