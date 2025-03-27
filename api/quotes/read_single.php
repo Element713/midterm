09c7be162e468cccf3b0d8e376aba86cf5e91d74
Shadow Love-Erckert <srlove@mail.fhsu.edu>
2025-03-27 14:38:06 -0500

    Requested Update

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -3,7 +3,6 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-include_once '../../config/Database.php';
 include_once '../../models/Quote.php';
 
 // Instantiate DB & connect
